a2bd14a073f9020d8966321b6875084b6066d6bc
drip016 <73792907+drip016@users.noreply.github.com>
2022-04-07 12:58:45 +0200

    style: Add chain icon with style, refactor files and more style adjustments

--- page/scripts/script.js
+++ page/scripts/script.js
@@ -4,7 +4,8 @@ const scrollBtn = document.querySelector('.scroll-top');
 const observer = new IntersectionObserver(function (entries) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      scrollBtn.style.bottom = '-5.8em';
+      scrollBtn.style.bottom = '-4em';
+      scrollBtn.style.opacity = '0';
     } else {
       scrollBtn.style.bottom = '1.8em';
       scrollBtn.style.opacity = '1';
