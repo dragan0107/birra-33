dfc3422bb5d2e6671605867be6290c5b9b1db7ff
drip016 <73792907+drip016@users.noreply.github.com>
2022-04-08 15:02:46 +0200

    bugfix: Fix a bug where submenus would appear outside of navbar + some refactor

--- page/scripts/script.js
+++ page/scripts/script.js
@@ -2,15 +2,29 @@ const navibar = document.querySelector('.navibar-wrapper');
 const scrollBtn = document.querySelector('.scroll-top');
 
 const observer = new IntersectionObserver(function (entries) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      scrollBtn.style.bottom = '-4em';
-      scrollBtn.style.opacity = '0';
-    } else {
-      scrollBtn.style.bottom = '1.8em';
-      scrollBtn.style.opacity = '1';
-    }
-  });
+  const entry = entries[0];
+  console.log(entry);
+  if (entry.isIntersecting) {
+    scrollBtn.style.bottom = '-4em';
+    scrollBtn.style.opacity = '0';
+  } else {
+    scrollBtn.style.bottom = '1.8em';
+    scrollBtn.style.opacity = '1';
+  }
 });
 
 observer.observe(navibar);
+
+window.onscroll = function () {
+  addNaviClass();
+};
+
+var sticky = navibar.offsetTop;
+
+function addNaviClass() {
+  if (window.pageYOffset > sticky) {
+    navibar.classList.add('navibar-sticky');
+  } else {
+    navibar.classList.remove('navibar-sticky');
+  }
+}
