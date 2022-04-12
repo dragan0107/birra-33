8fdf354418876e016773426d154a5bded578a537
drip016 <73792907+drip016@users.noreply.github.com>
2022-04-12 16:00:33 +0200

    bugfix: Update pagination with some bugfixing

--- page/scripts/fetch-pagination.js
+++ page/scripts/fetch-pagination.js
@@ -1,3 +1,4 @@
+let currentPage = 1; // Page initialization.
 let source = document.getElementById('beer-template').innerHTML;
 let template = Handlebars.compile(source);
 
@@ -12,14 +13,32 @@ const fetchBeers = (page, amount) => {
     });
 };
 
-fetchBeers(6, 10);
+// Initial beer fetch
+fetchBeers(1, 10);
 
-let currentPage = 1;
 const nums = document.getElementsByClassName('page-values');
 for (let i = 0; i < nums.length; i++) {
   nums[i].addEventListener('click', (e) => {
     const pageNum = e.target.innerHTML;
+    let activeElem = document.getElementsByClassName('page-item active');
+    let pageNumber = document.querySelectorAll('.page-number');
+    activeElem[0].classList.remove('active'); // Remove the currently active element class
     fetchBeers(pageNum, 10);
+    // Logic for adding the correct active class to the currently clicked page as well as incrementing or decrementing it in the dom.
+    if (pageNum > currentPage) {
+      currentPage = pageNum * 1;
+      pageNumber[0].classList.add('active');
+    } else if (currentPage === 1) {
+      pageNumber[0].classList.add('active');
+    } else {
+      currentPage = pageNum * 1 - 1;
+      pageNumber[1].classList.add('active');
+    }
+    if (currentPage < 1) {
+      currentPage = 1;
+    }
+
+    generatePageNums();
   });
 }
 
@@ -27,8 +46,10 @@ function changePage(val) {
   val === '+' ? currentPage++ : currentPage--;
   if (currentPage < 1) currentPage = 1;
   generatePageNums();
+  fetchBeers(currentPage, 10);
 }
 
+// After manipulating with current page, we refresh the values in the dom with this function.
 function generatePageNums() {
   nums[0].innerHTML = currentPage;
   nums[1].innerHTML = currentPage + 1;
