d2c1a106817c5a1a4bad15ba52a62f4ff1687d98
drip016 <73792907+drip016@users.noreply.github.com>
2022-04-14 16:24:02 +0200

    wip: Shop cart refactor and small fix

--- page/scripts/addToCart.js
+++ page/scripts/addToCart.js
@@ -1,15 +1,25 @@
 let beerItemSource = document.getElementById('cart-item').innerHTML;
 let shopItemsTemplate = Handlebars.compile(beerItemSource);
 
+// Helper to stringify the object passed in the function.
 Handlebars.registerHelper('json', function (context) {
   return JSON.stringify(context);
 });
-let shoppingCart = [];
+let shoppingCart = JSON.parse(localStorage.getItem('cart'));
+let cartQuantity = 0;
 
+//Initial cart quantity calculation and shop cart render.
+shoppingCart.forEach((item) => {
+  cartQuantity += +item.quantity;
+});
+renderCart();
+
+// Temporary object where we store the last purchased item.
 let shopItem = {};
 
 function addItem(item) {
   shopItem.purchasedItem = item;
+  // To prevent quantity of 0 from being added to the cart.
   if (shopItem.quantity) {
     shoppingCart.push(shopItem);
     localStorage.setItem('cart', JSON.stringify(shoppingCart));
@@ -17,17 +27,15 @@ function addItem(item) {
     renderCart();
   }
 }
-
-function renderCart() {
-  let cartItems = JSON.parse(localStorage.getItem('cart'));
-  const totalItems = document.querySelector('.cart-amount');
-  totalItems.innerHTML = `YOUR CART: ${cartItems.length} items`;
-  document.getElementById('beer-shop-cart').innerHTML = shopItemsTemplate({
-    beeritems: cartItems,
-  });
-}
-renderCart();
-
 function changeQuantity(val) {
   shopItem.quantity = val;
+  cartQuantity += +val;
+}
+
+function renderCart() {
+  const totalItems = document.querySelector('.cart-amount');
+  totalItems.innerHTML = `YOUR CART: ${cartQuantity} items`;
+  document.getElementById('beer-shop-cart').innerHTML = shopItemsTemplate({
+    beeritems: shoppingCart,
+  });
 }
